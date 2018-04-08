894690da9f370145caeb1b4ff2efd07fc17567ac
mcty <tyler.macneil@my.ccsu.edu>
2018-04-08 12:24:09 -0400

    UDPServer update with SEQ confirming TM

--- UDPServer.java
+++ UDPServer.java
@@ -47,6 +47,7 @@ public class UDPServer extends Host {
   
   public void run(){
     String messageT = "";
+	int SEQ_previous = 1;
 	try{
       receivingSocket = new DatagramSocket(port);
       System.out.println("Host is listening for UDP data on port " + port
@@ -78,15 +79,14 @@ public class UDPServer extends Host {
 		String userData = arr[5];
 		userData = userData.substring(0, userData.length() - 2);
 		
-		messageT = messageT + userData;
-		if(EOM == 1){
-			printDataReceived(SEQ,EOM,SenderIP,messageT);
-			
+		if(SEQ != SEQ_previous){
+			messageT = messageT + userData;
+			if(EOM == 1){
+				printDataReceived(SEQ,EOM,SenderIP,messageT);
+			}
+			sendACK(receivingSocket,SEQ,packet.getAddress(), packet.getPort());
+			SEQ_previous = SEQ;
 		}
-		
-		//Send ACK to sender.
-		
-		sendACK(receivingSocket, SEQ, packet.getAddress(), packet.getPort());
       }
     }
     catch(Exception e){
@@ -101,64 +101,4 @@ public class UDPServer extends Host {
 	  DatagramPacket packet = new DatagramPacket(packetData, packetData.length, SenderIP, SenderPort);
       socket.send(packet);
   }
-/* 
-  //Send an amount of data to the target machine
-  public void sendData(byte[] data) throws SocketException, IOException, InterruptedException{
-   
-    //Data to put in Application-Header
-    String method = "inform"; //TODO: need to allow client program to specify method
-    String hostName = getHostName();
-    String hostIP = getIPAddress();
-    boolean SEQ = false; //Sequence number (0/false or 1/true
-    boolean EOM = false; //End of message flag (0-->more data soon, 1-->last packet)
-    
-    //Application-Header String
-    String CRLF = "\r\n";
-    String firstRow = method + " " + hostName + " " + hostIP + CRLF;
-    
-    //Amounts of data to send
-    int headerChars = firstRow.length() + "0 0\r\n".length() + CRLF.length();
-    int availableBufSize = bufSize - headerChars; //This is the only amount we can send. If less than 0, no data can be sent at all
-    if(availableBufSize <= 0){
-      System.out.println("No data can be sent using the current configuration -- Increase Buffer Size / MTU");
-      return;
-    }
-    
-    //Send data!
-    ByteArrayInputStream byteStream = new ByteArrayInputStream(data);
-    int packetNum = 0;
-    
-    System.out.println("Starting message!");
-    while(byteStream.available()>0){ //While data remains
-      byte[] appDataBuffer = new byte[availableBufSize]; //Read data into buffer
-      int bytesRead = byteStream.read(appDataBuffer);
-      
-      //If entire buffer not filled, only get array containing data
-      if(bytesRead< availableBufSize){ 
-        appDataBuffer = Arrays.copyOf(appDataBuffer, bytesRead);
-      }
-      
-      //If there are no more bytes, set EOM flag
-      if(byteStream.available()==0) EOM = true;
-      
-      //Create packet's exact data
-      int packetSize = headerChars + bytesRead;
-      String secondRow = "" + (SEQ?1:0) + " " + (EOM?1:0) + CRLF;
-      byte[] packetData = (firstRow + secondRow + new String(appDataBuffer) + CRLF).getBytes();
-      
-      //Send packet
-      System.out.println(
-              "\n\n| - - - - START PACKET (" + packetNum++ + ") - - - - - - - - |\n"
-              + new String(packetData) 
-              + "|- - - - END PACKET (total length: " + packetSize + ") - - - - -| \n");
-      DatagramPacket packet = new DatagramPacket(packetData, packetData.length, targetIPAddr, targetPort);
-      socket.send(packet);
-      SEQ = !SEQ;
-      
-      Thread.sleep(1200);
-    }
-    
-    System.out.println("Full message sent!");
-  }
-*/  
 }
